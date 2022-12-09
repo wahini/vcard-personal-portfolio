62e20de43417133d0c0d4bc1de3d5e0d0ab2f463
Bhadrika Evandito Atmomintarso <42692320+Wahini@users.noreply.github.com>
2022-12-10 01:46:25 +0700

    minor

---
+++ assets/js/test1.js
@@ -0,0 +1,15 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('checkbox', () => {
+	document.body.classList.toggle('light');
+
+	// var x = document.getElementsByClassName(".info-content");
+	// var y = document.querySelector(".info-content > h1");
+	// console.log("halo");
+	// if (x.innerHTML === y, "Vito Atmo") {
+	//   x.innerHTML = "Wahini";
+	// } else {
+	//   x.innerHTML = "Vito Atmo";
+	// }
+});
+
